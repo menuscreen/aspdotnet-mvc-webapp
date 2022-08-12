862761edda4769f00d22aa17a2b07159cbab71de
menuscreen <70410703+menuscreen@users.noreply.github.com>
2022-08-12 15:31:28 -0400

    Add project files.

---
+++ aspdotnet-mvc-webapp/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace aspdotnet_mvc_webapp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
